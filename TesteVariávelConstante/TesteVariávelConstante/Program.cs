86fcf7188266f9d3740ef6f602827103ba77ca05
izahonorato <izahonorato@gmail.com>
2021-04-27 22:37:06 -0300

    primeiro commit c#
    primeiro commit c#

---
+++ TesteVariávelConstante/TesteVariávelConstante/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TesteVariávelConstante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //comentario de linha
+        /*comentario varias linhas*/
+            const string constante = "uma constante que não muda";
+
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Favor informar um número inteiro");
+                return;
+            }
+
+            int inteiro;
+            bool teste = int.TryParse(args[0], out inteiro);
+
+            if(teste == false)
+            {
+                Console.WriteLine("O argumento informado não é um inteiro");
+                return;
+            }
+
+            Console.WriteLine(constante);
+            Console.WriteLine("O número informado foi o: " + inteiro);
+        }
+    }
+}
