c3f1138d6bba46e8f62c8bbaa6dbe312204d1fa8
izahonorato <izahonorato@gmail.com>
2021-05-15 13:23:13 -0300

    Lista de animais utilizando classes

---
+++ ListaAnimais/ListaAnimais/Classes/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAnimais.Classes
+{
+    class Animal //classe pai que passa o atributo nome como herança
+    {
+        internal Mamifero qtdeMamas;
+
+        public string Nome { get; set; }
+    }
+    class Mamifero : Animal //os dois pontos dizem que mamifero é classe filha de animal
+    {
+        public int qtdeMamas { get; set; }
+    }
+
+    class Reptil : Animal
+    {
+        public bool ControlaTemperaturaCorpo { get; set; } = false;
+    }
+
+    class Ave : Animal
+    {
+        public bool TemPena { get; set; } = true;
+    }
+
+    class Peixe : Animal
+    {
+        public int qtdeNadadeiras { get; set; }
+    }
+}
---
+++ ListaAnimais/ListaAnimais/Program.cs
@@ -0,0 +1,53 @@
+﻿using ListaAnimais.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListaAnimais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Favor digitar o nome do animal:");
+            var nomeAnimal = Console.ReadLine();
+            if(string.IsNullOrEmpty(nomeAnimal) || string.IsNullOrWhiteSpace(nomeAnimal))
+            {
+                Console.WriteLine("Favor passar um nome de animal vertebrado válido!");
+                return;
+            }
+
+            var listaAnimais = new List<Animal>();
+            listaAnimais.Add(new Mamifero() { Nome = "Vaca", qtdeMamas = 4 });
+            listaAnimais.Add(new Reptil() { Nome = "Cobra" });
+            listaAnimais.Add(new Ave() { Nome = "Gavião" });
+            listaAnimais.Add(new Peixe() { Nome = "Tubarão", qtdeNadadeiras = 2 });
+
+            var animalSelecionado = listaAnimais.FirstOrDefault(animal => animal.Nome.ToUpper() == nomeAnimal.ToUpper());
+
+            if(animalSelecionado == null)
+            {
+                Console.WriteLine("Animal informado não está na nossa lista");
+                return;
+            }
+
+            if(animalSelecionado is Mamifero)
+            {
+                Console.WriteLine($"O animal encontrado é um mamífero com nome {animalSelecionado.Nome} e quantidade de mamas = {((Mamifero)animalSelecionado).qtdeMamas}");
+            }else if(animalSelecionado is Reptil)
+            {
+                Console.WriteLine($"O animal encontrado é um réptil com nome {animalSelecionado.Nome} e não controla sua temperatura = {((Reptil)animalSelecionado).ControlaTemperaturaCorpo}");
+            }else if(animalSelecionado is Ave)
+            {
+                Console.WriteLine($"O animal encontrado é uma ave com nome {animalSelecionado.Nome} e tem penas = {((Ave)animalSelecionado).TemPena}");
+            }else if(animalSelecionado is Peixe)
+            {
+                Console.WriteLine($"O animal encontrado é um peixe com nome {animalSelecionado.Nome} e tem nadadeiras = {((Peixe)animalSelecionado).qtdeNadadeiras}");
+            }
+            else
+            {
+                Console.WriteLine($"O animal encontrado é um animal e tem nome " + $"{animalSelecionado}");
+            }
+        }
+    }
+}
