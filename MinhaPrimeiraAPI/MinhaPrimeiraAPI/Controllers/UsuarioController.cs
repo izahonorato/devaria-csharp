e68273c62ad1ba959cd7e6902fd763f3c1a8948a
izahonorato <izahonorato@gmail.com>
2021-05-18 12:42:48 -0300

    Primeira API em C#

---
+++ MinhaPrimeiraAPI/MinhaPrimeiraAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObterUsuario()
+        {
+            return Ok("Olá mundo. Meu primeiro endpoint C#");
+        }
+    }
+}
